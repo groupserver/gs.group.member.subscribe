e4d86a63f31f8f4a32392ac1eb069b438e8568d5
Michael JasonSmith <mpj17@onlinegroups.net>
2015-05-29 17:01:47 -0700

    Binary strings, and a bump for the version

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -1,6 +1,11 @@
 Changelog
 =========
 
+1.0.1 (2015-05-29)
+------------------
+
+* Switching to Unicode literals for the faux data used in the
+  tests
 
 1.0.0 (2014-10-03)
 ------------------
--- gs/group/member/subscribe/tests/faux.py
+++ gs/group/member/subscribe/tests/faux.py
@@ -1,15 +1,30 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2014, 2015 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+from __future__ import absolute_import, print_function, unicode_literals
 from email.parser import Parser
 from gs.group.list.command.tests.faux import FauxGroup  # lint:ok
 
 
 class FauxSiteInfo(object):
     name = 'An Example Site'
-    id = 'example'
+    id = b'example'
 
 
 class FauxGroupInfo(object):
     name = 'An Example Group'
-    id = 'example_group'
+    id = b'example_group'
     url = 'https://lists.example.com/groups/example_group'
     siteInfo = FauxSiteInfo()
     groupObj = 'This is not a folder'
@@ -17,7 +32,7 @@ class FauxGroupInfo(object):
 
 class FauxUserInfo(object):
     name = 'An Example user'
-    id = 'exampleuser'
+    id = b'exampleuser'
 
 
 class FauxVisibility(object):
@@ -26,19 +41,19 @@ class FauxVisibility(object):
 
 def faux_email(subject='join'):
     retval = Parser().parsestr(
-        'From: <member@example.com>\n'
-        'To: <group@example.com>\n'
-        'Subject: {0}\n'
-        '\n'
-        'Body would go here\n'.format(subject))
+        b'From: <member@example.com>\n'
+        b'To: <group@example.com>\n'
+        b'Subject: {0}\n'
+        b'\n'
+        b'Body would go here\n'.format(subject))
     return retval
 
 
 class FauxConfirmation(object):
 
-    def __init__(self, context=None, email='a.person@example.com',
-                 confirmationId='a0b1c2', userId='durk',
-                 groupId='example_group', siteId='example'):
+    def __init__(self, context=None, email=b'a.person@example.com',
+                 confirmationId=b'a0b1c2', userId=b'durk',
+                 groupId=b'example_group', siteId=b'example'):
         self.context = context
         self.email = email
         self.confirmationId = confirmationId
